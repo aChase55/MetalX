193d52b745f42853a3365a9c7b228d96a3b03f13
Alex Chase <alexchase55@gmail.com>
2025-07-08 13:28:52 -0700

    Initial Commit

---
+++ MetalX/MetalXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalXApp.swift
+//  MetalX
+//
+//  Created by Alex Chase on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetalXTests/MetalXTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetalXTests.swift
+//  MetalXTests
+//
+//  Created by Alex Chase on 7/8/25.
+//
+
+import Testing
+@testable import MetalX
+
+struct MetalXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
